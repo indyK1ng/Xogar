ab17e2362219663c35fc821af0dcd39965a64299
Indy <mnuzzo.se@gmail.com>
2014-04-18 23:15:02 -0400

    Committing while I go fix something that is broken.

---
+++ XogarLib/XmlDataInterface.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace XogarLib
+{
+    public class XmlDataInterface <T> where T : new()
+    {
+        private string _folderPath;
+
+        public XmlDataInterface (string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+
+        public void Save()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            FileStream stream = new FileStream(_folderPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            serializer.Serialize(stream, this);
+        }
+
+        public T Load()
+        {
+            XmlSerializer deserialize = new XmlSerializer(typeof(T));
+
+            if (File.Exists(_folderPath))
+            {
+                FileStream stream = new FileStream(_folderPath, FileMode.Open, FileAccess.Read);
+                T gameSelectLists = (T)deserialize.Deserialize(stream);
+                stream.Close();
+
+                return gameSelectLists;
+            }
+
+            return new T();
+        }
+    }
+}
